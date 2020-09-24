70d3929e148f36f35c67556c7896edc538914e4e
ozofweird <ozofweird@gmail.com>
2020-09-25 01:17:56 +0900

    server_test

--- scripts/health.sh
+++ scripts/health.sh
@@ -7,7 +7,7 @@ source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
 
-echo "> Health Check 시"
+echo "> Health Check 시작"
 echo "> IDLE_PORT: $IDLE_PORT"
 echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
